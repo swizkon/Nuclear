c146ff279c5f1e0ac3be9fe7dc847b3e1bd70012
Jonas Jerndin <jonas.jerndin@ne.se>
2016-02-29 23:25:59 +0100

    New MySql impl.

---
+++ Nuclear.NET/Nuclear.EventSourcing.MySql/EventData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nuclear.EventSourcing.MySql
+{
+    public sealed class EventData
+    {
+
+        public readonly byte[] Data;
+
+        public readonly Guid EventId;
+
+        public readonly bool IsJson;
+
+        public readonly byte[] Metadata;
+
+        public readonly string Type;
+
+        public EventData(Guid eventId, string type, bool isJson, byte[] data, byte[] metadata)
+        {
+            this.EventId = eventId;
+            this.Type = type;
+            this.IsJson = isJson;
+            this.Data = data;
+            this.Metadata = metadata;
+        }
+    }
+}
---
+++ Nuclear.NET/Nuclear.EventSourcing.MySql/MySqlEventSource.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Nuclear.Domain;
+using Nuclear.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuclear.EventSourcing.MySql
+{
+    public class MySqlEventSource : IAggregateEventStore
+    {
+        private const string EventClrTypeHeader = "EventClrTypeName";
+        private const string AggregateClrTypeHeader = "AggregateClrTypeName";
+        private const string CommitIdHeader = "CommitId";
+        private const int WritePageSize = 500;
+        private const int ReadPageSize = 500;
+
+        public void SaveEvents(Aggregate aggregate, Guid aggregateId, IEnumerable<Event> events)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetEventsForAggregate(Aggregate aggregate, Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private static class JsonEventData
+        {
+            public static EventData Create(Guid eventId, object evnt, IDictionary<string, object> headers)
+            {
+                var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(evnt, SerializerSettings));
+                var metadata = AddEventClrTypeHeaderAndSerializeMetadata(evnt, headers);
+                var typeName = evnt.GetType().Name;
+
+                return new EventData(eventId, typeName, true, data, metadata);
+            }
+
+            private static byte[] AddEventClrTypeHeaderAndSerializeMetadata(object evnt, IDictionary<string, object> headers)
+            {
+                var eventHeaders = new Dictionary<string, object>(headers)
+                {
+                    {EventClrTypeHeader, evnt.GetType().AssemblyQualifiedName}
+                };
+
+                return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(eventHeaders, SerializerSettings));
+            }
+
+            private static readonly JsonSerializerSettings SerializerSettings;
+
+            static JsonEventData()
+            {
+                SerializerSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.None };
+            }
+        }
+
+    }
+}
