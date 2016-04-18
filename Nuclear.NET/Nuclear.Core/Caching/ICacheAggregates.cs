fbdda2ef13f2a791483bc1f76fa198e6b282c35c
Jonas Jerndin <jonas.jerndin@ne.se>
2016-04-18 22:10:13 +0200

    New console to be able to run some dev code.

--- Nuclear.NET/Nuclear.Core/Caching/ICacheAggregates.cs
+++ Nuclear.NET/Nuclear.Core/Caching/ICacheAggregates.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace Nuclear.Caching
 {
+    /*
     public interface ICacheAggregates<TAggregate>
         where TAggregate : Aggregate
     {
         TAggregate Get(AggregateKey key);
+
         void Insert(TAggregate aggregate);
     }
+    */
 }
---
+++ Nuclear.NET/Nuclear.TestConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuclear.TestConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cfg = new Configuration()
+                .DataBaseIntegration(db =>
+                {
+                    db.ConnectionString = "";
+                    db.Dialect<MySQLDialect>();
+                });
+
+            /* Add the mapping we defined: */
+            var mapper = new ModelMapper();
+            mapper.AddMapping(typeof(Nuclear.EventSourcing.MySql.RecordedEventMap));
+
+            HbmMapping mapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+            cfg.AddMapping(mapping);
+
+
+
+            string outfile = System.Environment.CurrentDirectory + "\\Nuclear.EventSourcing.Schema.mysql";
+
+            Console.WriteLine(outfile);
+
+
+            StringBuilder data = new StringBuilder();
+
+            SchemaExport schema = new SchemaExport(cfg);
+
+            schema.SetOutputFile(outfile)
+                .Create((a) =>
+                {
+                    data.AppendLine(a);
+                }, false);
+
+
+            System.IO.File.WriteAllText(outfile, data.ToString());
+
+            schema.SetOutputFile(outfile).Create(false, false);
+            // schema.Create(true, false);
+
+            Console.ReadKey();
+        }
+    }
+}
