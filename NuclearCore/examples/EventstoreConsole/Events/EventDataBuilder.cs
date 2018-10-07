e4a962558cdc0a478db8d031e7122998c70b9af3
swizkon <jonas@jerndin.se>
2018-10-07 08:18:51 +0200

    XDD stuff for initial ES connection

---
+++ NuclearCore/examples/EventstoreConsole/Events/EventDataBuilder.cs
@@ -0,0 +1,56 @@
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EventStore.ClientAPI;
+using Newtonsoft.Json;
+
+namespace EventstoreConsole.Events
+{
+
+    public class TaskAdded
+    {
+        public string Name { get; set; }
+    }
+
+    public static class EventDataBuilder
+    {
+
+        private const string EventClrTypeHeader = "EventClrTypeName";
+        private const string AggregateClrTypeHeader = "AggregateClrTypeName";
+        private const string CommitIdHeader = "CommitId";
+        
+            public static EventData BuildTaskAddedEvent(string v)
+            {
+                var metadata = new Dictionary<string, string>();
+                
+                var t = new TaskAdded {Name = v};
+                return Create(Guid.NewGuid(), t, metadata);
+            }
+
+             private static EventData Create(Guid eventId, object evnt, IDictionary<string, string> headers)
+            {
+                var SerializerSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.None };
+                var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(evnt, SerializerSettings));
+                
+                var metadata = AddEventClrTypeHeaderAndSerializeMetadata(evnt, headers);
+                var typeName = evnt.GetType().Name;
+
+                return new EventData(eventId, typeName, true, data, metadata);
+            }
+
+
+            private static byte[] AddEventClrTypeHeaderAndSerializeMetadata(object evnt, IDictionary<string, string> headers)
+            {
+                var SerializerSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.None };
+                var eventHeaders = new Dictionary<string, string>(headers)
+                {
+                    {EventClrTypeHeader, evnt.GetType().AssemblyQualifiedName}
+                };
+
+                return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(eventHeaders, SerializerSettings));
+            }
+            
+    }
+
+}
---
+++ NuclearCore/examples/EventstoreConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using EventStore.ClientAPI;
+
+namespace EventstoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // docker run --name eventstore-node -it --rm -p 2113:2113 -p 1113:1113 eventstore/eventstore
+
+            Console.WriteLine("Hello World!");
+
+            // ConnectionSettings.Default.g
+
+            var conn = ConnectionSettings.Default;
+
+            var uri = new Uri("tcp://localhost:1113");
+
+            try
+            {
+                var connection = EventStoreConnection.Create(uri);
+
+                System.Console.WriteLine("Connect to Eventstore...");
+                connection.ConnectAsync().Wait();
+                System.Console.WriteLine("Connected!");
+
+                string name = null;
+
+                do{
+                    System.Console.WriteLine("Enter a task: (q to exit)");
+                        name = Console.ReadLine();
+                        
+                var evt = Events.EventDataBuilder.BuildTaskAddedEvent("jdaksdkajsdaskdj");
+
+                System.Console.WriteLine("Write to Eventstore...");
+                connection.AppendToStreamAsync("todos-jonas", ExpectedVersion.Any, evt).Wait();
+                System.Console.WriteLine("Written!!");
+
+                }
+                while(name != "q");
+
+
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
